292ec6e590d463c098f48d8c9aba12d49ceafd2e
Nehal Elkaref <karefsn@gmail.com>
2025-02-13 19:35:35 +0200

    fix:added binding and creation of db

--- app/src/__init__.py
+++ app/src/__init__.py
@@ -6,11 +6,13 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat_backup.db'
+    db.init_app(app)
 
     
     with app.app_context():
         from .routes import bp
         app.register_blueprint(bp)
-        db.init_app(app)
+        db.create_all()
+        
 
     return app
